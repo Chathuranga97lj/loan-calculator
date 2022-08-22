5d2d31acc27057244ea3d14ef6f736f30cdc391a
Chathuranga97lj <chathuit97@gmail.com>
2022-08-22 22:42:19 +0530

    add loading screen in when occour erros

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    // hide result
+    document.getElementById('results').style.display = 'none';
+
+    // show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault()
+});
 
 // calculate results
-function calculateResults(e) {
+function calculateResults() {
     console.log('calculating....');
 
     // ui variables
@@ -25,15 +35,26 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // show results
+        document.getElementById('results').style.display = 'block';
+
+        // hide loader
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check your numbers');
     }
 
-    e.preventDefault();
 }
 
 // show error
 function showError(error){
+    // hide result
+    document.getElementById('results').style.display = 'none';
+
+    // hide loader
+    document.getElementById('loading').style.display = 'none';
+
     // create div
     const errorDiv = document.createElement('div');
 
